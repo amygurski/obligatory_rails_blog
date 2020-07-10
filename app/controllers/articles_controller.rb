ed3586957274d1597a82bdca86f416127206cd4f
Amy Gurski <amygurski@gmail.com>
2020-07-10 14:14:42 -0400

    add new create edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,42 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+    #so first time page loads, have empty article variable that's empty for error messages
+  end
+
+  def create
+    #see that working before implementing
+    #render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
 end
